f9e75177b9cd14ddb340f22971ce2a53aeab2172
張旭 <zx1986@gmail.com>
2022-02-21 19:13:57 +0800

    fix: 改善 Notes 的建立流程
    - command 改成 set / get
    - 判斷同一發言人、同一頻道才記錄
    - 缺點是同一時間點只能建立一則筆記

--- plugins/err-notes/notes.py
+++ plugins/err-notes/notes.py
@@ -18,8 +18,9 @@ class Notes(BotPlugin):
         content = message.body.strip()
         username = message.frm
         channel = message.frm.room if message.is_group else message.frm
+        channel_str = '{}'.format(channel)
 
-        if not content or content.startswith("!"):
+        if not content:
             return
 
         if content == '#end':
@@ -28,20 +29,32 @@ class Notes(BotPlugin):
 
         if self.creating:
             key = self.creating
-            self.notes[key].append(content)
+            if self.notes[key]['author'] == username and self.notes[key]['channel'] == channel_str:
+                self.notes[key]['content'].append(content)
+            else:
+                pass # 發言人與作者不同，發言不在同一頻道，則不添加內容到筆記
+        else:
+            pass # creating 為空表示目前沒有任何筆記在創建狀態，缺點是同一時間只能有一筆在創建
 
     @botcmd
     def note_set(self, message, args):
         """Create a note"""
         key = args
+        username = message.frm
+        channel = message.frm.room if message.is_group else message.frm
+        channel_str = '{}'.format(channel)
+
         self.creating = key
         if key not in self.notes:
-            self.notes[key] = []
+            self.notes[key] = {}
+            self.notes[key]['author'] = username
+            self.notes[key]['channel'] = channel_str
+            self.notes[key]['content'] = []
         yield "開始記錄 {} ：".format(key)
 
     @botcmd(template="content")
     def note_get(self, message, args):
         """Read a note"""
         key = args
-        content = "\n".join(self.notes[key])
+        content = "\n".join(self.notes[key]['content'])
         return { 'key': key, 'content': content }
