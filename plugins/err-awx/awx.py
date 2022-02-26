b22ad6be054bc68db67b378c5a7223b1790d774f
張旭 <zx1986@gmail.com>
2022-02-27 00:04:12 +0800

    feat: 新增取得 AWX job template 列表以及發射工作的接口
    還要改得更優雅

--- plugins/err-awx/awx.py
+++ plugins/err-awx/awx.py
@@ -15,9 +15,35 @@ class Awx(BotPlugin):
 
     @botcmd(template="json")
     def awx_ping(self, message, args):
-        """Get AWX base info"""
+        """Get AWX base infomation."""
         process = subprocess.Popen(
                     ['awx', 'ping'],
+                    env=self.env,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    universal_newlines=True
+                )
+        stdout, stderr = process.communicate()
+        return { 'stdout': stdout, 'stderr': stderr }
+
+    @botcmd(template="json")
+    def awx_job_templates_launch(self, message, args):
+        """Launch AWX job by specified job template id."""
+        process = subprocess.Popen(
+                    ['awx', 'job_templates', 'launch', args],
+                    env=self.env,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    universal_newlines=True
+                )
+        stdout, stderr = process.communicate()
+        return { 'stdout': stdout, 'stderr': stderr }
+
+    @botcmd(template="json")
+    def awx_job_templates_list(self, message, args):
+        """Launch AWX job by specified job template id."""
+        process = subprocess.Popen(
+                    ['awx', 'job_templates', 'list'],
                     env=self.env,
                     stdout=subprocess.PIPE,
                     stderr=subprocess.PIPE,
