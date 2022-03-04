8ba5dccb0aa815a35943426744ced104b9694c0f
張旭 <zx1986@gmail.com>
2022-03-04 23:50:21 +0800

    feat: 編排一下函式順序，並新增 awx job get

--- plugins/err-awx/awx.py
+++ plugins/err-awx/awx.py
@@ -32,13 +32,21 @@ class Awx(BotPlugin):
         result = self._run_command(cmd)
         return result
 
-    @botcmd(template="json")
-    def awx_job_templates_launch(self, message, args):
-        """發射指定編號的 AWX 任務"""
-        # TODO: 建立互動詢問，確認是否執行，而不是無腦直接執行
-        cmd = ['awx', 'job_templates', 'launch', args]
+    @botcmd(template="markdown")
+    def awx_projects_list(self, message, args):
+        """條列 AWX 專案"""
+        # TODO: 根據 args 進行搜尋過濾
+        cmd =['awx', 'projects', 'list', '--all']
         result = self._run_command(cmd)
-        return result
+        projects = json.loads(result['stdout'])
+        content = []
+        for project in projects['results']:
+            content.append("\n")
+            content.append("專案編號：{}".format(project['id']))
+            content.append("專案名稱：{}".format(project['name']))
+            content.append("專案源碼：{}".format(project['scm_url']))
+            content.append("專案版本：{}".format(project['scm_revision']))
+        return { 'content': "\n".join(content) }
 
     @botcmd(template="raw")
     def awx_job_templates_list(self, message, args):
@@ -55,18 +63,17 @@ class Awx(BotPlugin):
             content.append("任務腳本：{}".format(job_template['playbook']))
         return { 'content': "\n".join(content) }
 
-    @botcmd(template="markdown")
-    def awx_projects_list(self, message, args):
-        """條列 AWX 專案"""
-        # TODO: 根據 args 進行搜尋過濾
-        cmd =['awx', 'projects', 'list', '--all']
+    @botcmd(template="json")
+    def awx_job_templates_launch(self, message, args):
+        """發射指定編號的 AWX 任務"""
+        # TODO: 建立互動詢問，確認是否執行，而不是無腦直接執行
+        cmd = ['awx', 'job_templates', 'launch', args]
         result = self._run_command(cmd)
-        projects = json.loads(result['stdout'])
-        content = []
-        for project in projects['results']:
-            content.append("\n")
-            content.append("專案編號：{}".format(project['id']))
-            content.append("專案名稱：{}".format(project['name']))
-            content.append("專案源碼：{}".format(project['scm_url']))
-            content.append("專案版本：{}".format(project['scm_revision']))
-        return { 'content': "\n".join(content) }
+        return result
+
+    @botcmd(template="json")
+    def awx_job_get(self, message, args):
+        """取得指定編號的 AWX 任務狀態"""
+        cmd = ['awx', 'job', 'get', args]
+        result = self._run_command(cmd)
+        return result
